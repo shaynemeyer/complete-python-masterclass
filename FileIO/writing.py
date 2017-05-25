1922564de9c3c8ca37301ed459b1267da588c592
Shayne Meyer <shayne.meyer@gmail.com>
2017-05-25 07:32:27 -0700

    added imelda3

--- FileIO/writing.py
+++ FileIO/writing.py
@@ -4,13 +4,33 @@
 #     for city in cities:
 #         print(city, file=city_file)
 
-cities = []
+# cities = []
+#
+# with open('cities.txt', 'r') as city_file:
+#     for city in city_file:
+#         cities.append(city.strip('\n'))
+#
+# # print(cities)
+# for city in cities:
+#     print(city)
 
-with open('cities.txt', 'r') as city_file:
-    for city in city_file:
-        cities.append(city.strip('\n'))
+# imelda = "More Mayhem", "Imelda May", "2011", (
+#     (1, "Pulling the Rug"),
+#     (2, "Psycho"),
+#     (3, "Mayhem"),
+#     (4, "Kentish Town Waltz")
+# )
+#
+# with open("imelda3.txt", 'w') as imelda_file:
+#     print(imelda, file=imelda_file)
 
-print(cities)
-for city in cities:
-    print(city)
+with open("imelda3.txt", 'r') as imelda_file:
+    contents = imelda_file.readline()
 
+imelda = eval(contents)
+
+print(imelda)
+title, artist, year, tracks = imelda
+print(title)
+print(artist)
+print(year)
