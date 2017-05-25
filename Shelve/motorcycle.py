625dfccc6d1709b1d9dab55d8c45ac2fe8d831bf
Shayne Meyer <shayne.meyer@gmail.com>
2017-05-25 13:02:15 -0700

    updated motorcycle

--- Shelve/motorcycle.py
+++ Shelve/motorcycle.py
@@ -6,5 +6,10 @@ with shelve.open("bike") as bike:
     bike["color"] = "red"
     bike["engine_size"] = 250
 
+    for key in bike:
+        print(key)
+
+    print('=' * 40)
+
     print(bike["engine_size"])
     print(bike["color"])
